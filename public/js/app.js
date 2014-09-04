7096fe0b3db6728057794f50fbf9c540a1101fd2
Ben Wilhelm <bwilhelm@networkninja.com>
2014-09-04 07:46:35 -0500

    Start new story with no 311 ticket forms
    Tickets are not required, so we start with no form. Required adding the form as a
    handlebars template to have something to clone.

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
   $("#add_ticket").click(function(e){
     e.preventDefault();
-    var $group = $('#ticket_groups .ticket-group').first().clone();
-    $group.find("input, textarea").val("");
-    
+    var html = $("#new_ticket_template").html();
+    var $group = $(html);
     var idx = $("#ticket_groups .ticket-group").length;
     $group.find("input, textarea").each(function(i, o){
       var oldName = $(o).attr('name');
